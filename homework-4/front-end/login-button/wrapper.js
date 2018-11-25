c8d355f41251f70e1dbdf4172bd0427ab4e389ec
Unknown <julius.cc@hotmail.com>
2018-11-25 18:05:27 +0100

    Fixed react stuff

--- homework-4/front-end/login-button/wrapper.js
+++ homework-4/front-end/login-button/wrapper.js
@@ -2,7 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const showLoginButton = element => {
-	ReactDOM.render(<p>Hello!</p>, element)
+	ReactDOM.render(
+		<a
+			class="button button--place-right button--stretch-mobile navbar__item--center-vertically button--mobile-smaller"
+			href="http://localhost:8888/index.php/auth/logout"
+		>
+			Log out
+		</a>,
+		element
+	)
 }
 
 module.exports = showLoginButton
